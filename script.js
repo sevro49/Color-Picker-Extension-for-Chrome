956ff190ba1cc0472069dd9461eec094ef62f210
sevro49 <21120205303@ismu.edu.tr>
2023-04-15 03:52:56 +0300

    Storing & Fetching picked colors from local storage

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const colorPickerBtn = document.querySelector("#color-picker");
+const colorList = document.querySelector(".all-colors");
+const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
+
+const showColors = () => {
+    colorList.innerHTML = pickedColors
+        .map(
+            (color) => `
+        <li class="color">
+            <span class="rect" style="background: ${color}; border: 1px solid ${
+                color == "#ffffff" ? "#ccc" : color
+            }" ></span>
+            <span class="value">${color}</span>
+        </li>
+    `
+        )
+        .join(""); // Generation li for the picked color and adding it to the colorList
+};
+showColors();
 
 const activateEyeDropper = async () => {
     try {
@@ -6,7 +24,13 @@ const activateEyeDropper = async () => {
         const eyeDropper = new EyeDropper();
         const { sRGBHex } = await eyeDropper.open();
         navigator.clipboard.writeText(sRGBHex); // Copying the selected color to the clipboard
-        console.log(sRGBHex);
+
+        // Adding the color to the list if it doesn't already exist
+        if(!pickedColors.includes(sRGBHex)){
+            pickedColors.push(sRGBHex); // Adding selected colors to the pickedColors array
+            localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+            showColors();
+        }
     } catch (error) {
         console.log(error);
     }
